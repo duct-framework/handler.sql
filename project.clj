7fcc34a49852f491ca81dfd8dca413bcce4a2dfd
James Reeves <jreeves@weavejester.com>
2017-08-30 22:51:18 +0100

    Add Duct and Ring dependencies

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]])
+  :dependencies [[org.clojure/clojure "1.9.0-alpha19"]
+                 [org.clojure/java.jdbc "0.7.0"]
+                 [duct/core "0.6.1"]
+                 [integrant "0.6.1"]
+                 [ring/ring-core "1.6.2"]])
