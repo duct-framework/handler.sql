720054af9050235372cf85446dc195117b795784
James Reeves <jreeves@weavejester.com>
2017-09-08 02:12:00 +0100

    Add database boundary to handlers

--- src/duct/handler/sql.clj
+++ src/duct/handler/sql.clj
@@ -1,18 +1,26 @@
 (ns duct.handler.sql
   (:require [clojure.java.jdbc :as jdbc]
+            [duct.database.sql :as sql]
             [integrant.core :as ig]
             [ring.util.response :as resp]))
 
+(defprotocol RelationalDatabase
+  (query [db query]))
+
+(extend-protocol RelationalDatabase
+  duct.database.sql.Boundary
+  (query [{:keys [spec]} query] (jdbc/query spec query)))
+
 (defmethod ig/init-key ::select [_ {:keys [db request query] :or {request '_}}]
-  (let [f (eval `(fn [{spec# :spec}]
+  (let [f (eval `(fn [db#]
                    (fn [~request]
-                     (resp/response (jdbc/query spec# ~query)))))]
+                     (resp/response (query db# ~query)))))]
     (f db)))
 
 (defmethod ig/init-key ::select-one [_ {:keys [db request query] :or {request '_}}]
-  (let [f (eval `(fn [{spec# :spec}]
+  (let [f (eval `(fn [db#]
                    (fn [~request]
-                     (if-let [result# (first (jdbc/query spec# ~query))]
+                     (if-let [result# (first (query db# ~query))]
                        (resp/response result#)
                        (resp/not-found {:error :not-found})))))]
     (f db)))
