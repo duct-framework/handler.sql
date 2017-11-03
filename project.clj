fc61f282badfaaf3941077adac839855d52085c7
James Reeves <jreeves@weavejester.com>
2017-11-03 15:13:56 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,11 +3,11 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-beta1"]
-                 [org.clojure/java.jdbc "0.7.1"]
+  :dependencies [[org.clojure/clojure "1.9.0-beta4"]
+                 [org.clojure/java.jdbc "0.7.3"]
                  [duct/core "0.6.1"]
                  [duct/database.sql "0.1.0"]
                  [integrant "0.6.1"]
-                 [ring/ring-core "1.6.2"]]
+                 [ring/ring-core "1.6.3"]]
   :profiles
-  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.20.0"]]}})
+  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.20.1"]]}})
