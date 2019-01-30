164102f2fc8004f8d4b4b551521e0b3d7a8cd6b1
James Reeves <jreeves@weavejester.com>
2019-01-30 14:37:18 +0000

    Update dependencies

--- project.clj
+++ project.clj
@@ -3,13 +3,13 @@
   :url "https://github.com/duct-framework/handler.sql"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0-RC1"]
-                 [org.clojure/java.jdbc "0.7.3"]
-                 [duct/core "0.6.1"]
+  :dependencies [[org.clojure/clojure "1.10.0"]
+                 [org.clojure/java.jdbc "0.7.8"]
+                 [duct/core "0.7.0"]
                  [duct/database.sql "0.1.0"]
-                 [integrant "0.6.1"]
-                 [medley "1.0.0"]
-                 [ring/ring-core "1.6.3"]
-                 [uritemplate-clj "1.1.1"]]
+                 [integrant "0.7.0"]
+                 [medley "1.1.0"]
+                 [ring/ring-core "1.7.1"]
+                 [uritemplate-clj "1.2.1"]]
   :profiles
-  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.21.0"]]}})
+  {:dev {:dependencies [[org.xerial/sqlite-jdbc "3.25.2"]]}})
